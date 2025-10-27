ce908409276448d42d11c75bf286a697c58358b3
silas oj <silasojugo@gmail.com>
2025-10-27 08:05:04 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const router = require("./routes")
+require("dotenv").config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use("/countries", router)
+
+
+app.use((error, req, res, next)=>{
+    console.log("path: ", req.path)
+   
+    const StatusCode = error.status || 500
+    const message = error.message || "Internal server error"
+
+    return res.status(StatusCode).json({
+        error : message
+    })
+    // next()
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`)
+})
